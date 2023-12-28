307485c46a9e1d159c951f49d9a9eef41b76ae54
Krzysztof Wróblewski <k.wroblewski@dasl.pl>
2023-12-28 22:48:16 +0100

    silicate animation removed

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "66a3683dc130f3fce3662c6293cf83b4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "72be2627993abbdc3ef05f7c7b477eb5",
-"/": "72be2627993abbdc3ef05f7c7b477eb5",
-"main.dart.js": "ae65d79e44a1d316e81668729b566491",
+"index.html": "42248f4042c4e9de2fd70927c8573f2c",
+"/": "42248f4042c4e9de2fd70927c8573f2c",
+"main.dart.js": "b46efd3acce78b1ce3c64d142eb8a6ff",
 "manifest.json": "1f0e05ee64ff49cef0090a0f19a199d1",
 "version.json": "58b7ad0698d3ed964c82541e3bff2f77"};
 // The application shell files that are downloaded before a service worker can
