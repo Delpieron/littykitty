c7ae9b2d6e7341ce4e1d3eaccd70b2abae7dae0d
Krzysztof Wróblewski <k.wroblewski@dasl.pl>
2023-12-28 23:50:45 +0100

    app test

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "66a3683dc130f3fce3662c6293cf83b4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "86bb8ed683340e83b5148bfec3f6ccf0",
-"/": "86bb8ed683340e83b5148bfec3f6ccf0",
-"main.dart.js": "dbc2023279aa7118eff46ddff47527b0",
+"index.html": "79ac602f67043fc721af5c7769986df4",
+"/": "79ac602f67043fc721af5c7769986df4",
+"main.dart.js": "123bad8bf238c817e78ebe1d13da6d44",
 "manifest.json": "1f0e05ee64ff49cef0090a0f19a199d1",
 "version.json": "58b7ad0698d3ed964c82541e3bff2f77"};
 // The application shell files that are downloaded before a service worker can
